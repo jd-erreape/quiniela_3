32ec3bdbc88fc49ba8036b884fbda9364f64c1e7
jd-erreape <juandediosherrero@gmail.com>
2021-08-23 18:10:11 +0200

    Initial commit

---
+++ lib/quiniela.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quiniela/version"
+
+module Quiniela
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/quiniela/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Quiniela
+  VERSION = "0.1.0"
+end
